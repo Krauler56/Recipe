692b0abdbb5719dfc0fd69a267d97ceba5b03fd2
Valentin24 <valentinnacharov@yahoo.com>
2018-04-04 08:59:05 +0200

    Add RecipeTittleAddViewController + camera functionality

---
+++ Kitchen recipes/Cell/Recipe Add Cells/RecipeTitltleAddCell.swift
@@ -0,0 +1,24 @@
+//
+//  RecipeTitltleAddCell.swift
+//  Kitchen recipes
+//
+//  Created by Valentin Nacharov on 02.04.2018.
+//  Copyright © 2018 Valentin Nacharov. All rights reserved.
+//
+
+import UIKit
+
+class RecipeTitltleAddCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Kitchen recipes/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  Kitchen recipes
+//
+//  Created by Valentin Nacharov on 02.04.2018.
+//  Copyright © 2018 Valentin Nacharov. All rights reserved.
+//
+
+import Foundation
